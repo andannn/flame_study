6292df1492e0e49813d2321492a7cb80a5083193
andannn <jqn296763005@gmail.com>
2024-01-28 22:21:22 +0900

    add resource

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -6,4 +9,23 @@ void main() {
 }
 
 class MyGame extends FlameGame {
+  SpriteComponent girl = SpriteComponent();
+
+  @override
+  FutureOr<void> onLoad() async {
+    print("Screen size is $size");
+
+    girl
+      ..sprite = await loadSprite("image.png")
+      ..size = Vector2(100, 100)
+      ..y = 100;
+
+    add(girl);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+  }
 }
